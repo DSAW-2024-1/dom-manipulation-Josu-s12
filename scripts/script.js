a9d9e370c0144636c681f80f2ff6444ac8c25e3c
Josue Sarmiento <josuesagu@unisabana.edu.co>
2024-02-21 12:09:23 -0500

    falta Js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript working!");
+const fecha=document.querySelector("#fecha")
+const lista=document.querySelector("#lista")
+const input=document.querySelector("#input")
+const botonEnter=document.querySelector("#boton-enter")
+
+//agregar tarea
+function agregarTarea(tarea){
+    const elemento= `<i class="far fa-circle co" id="realizado" id="0"></i>
+                    <p class="text">${tarea}</p>
+                    <i class="fas fa-trash de" data="eliminado" id="0"></i>`
+
+    lista.insertAdjacentHTML("beforeend",elemento)
+}
+
+
+botonEnter.addEventListener("click", ()=>{
+    const tarea=input.value
+    if(tarea){
+        agregarTarea(tarea)
+        input.value=''
+    }
+})
